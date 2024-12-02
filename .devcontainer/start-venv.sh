b5be716c654713c0a3bf835b80be7a108ed8404e
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2024-12-02 15:30:06 +0000

    up

--- .devcontainer/start-venv.sh
+++ .devcontainer/start-venv.sh
@@ -1,9 +1,22 @@
 #! /bin/bash
 
-if [ ! -d $HOME/gmsh-env ]; then
-    python -m venv --system-site-packages $HOME/gmsh-env &&\
-    . $HOME/gmsh-env/bin/activate && \
-    python -m pip install gmsh
+set +x
+
+# pwd shall contains requirements.txt
+
+VENVDIR=./magnetrun-env
+USE_SYSTEM_PACKAGES=1
+
+if [ ! -d $VENVDIR ]; then
+   echo "create Python Virtualenv: VENVDIR={VENVDIR}"
+   if [ "$USE_SYSTEM_PACKAGES" == "1" ]; then
+      python -m venv --system-site-packages $VENVDIR
+   else
+      python -m venv $VENVDIR
+   fi
+   . $VENVDIR/bin/activate
+   python -m pip install -r requirements.txt
+   deactivate
 fi
 
 # add option to properly quit gmsh-env using deactivate
