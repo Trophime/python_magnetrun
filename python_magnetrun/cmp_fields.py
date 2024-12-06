4dad8a7be42e90efe767687fbf24c54e75927efc
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2024-12-06 14:11:42 +0000

    use paag for large dataset

--- python_magnetrun/cmp_fields.py
+++ python_magnetrun/cmp_fields.py
@@ -12,6 +12,7 @@ from scipy import stats
 from tabulate import tabulate
 
 # import logging
+from natsort import natsorted
 
 import numpy as np
 import pandas as pd
@@ -58,12 +59,32 @@ if __name__ == "__main__":
         help="select ykey",
         default="IB",
     )
+    parser.add_argument(
+        "--lagcorrelation", help="save graphs (png format)", action="store_true"
+    )
+    parser.add_argument("--dtw", help="save graphs (png format)", action="store_true")
     parser.add_argument("--save", help="save graphs (png format)", action="store_true")
+    parser.add_argument("--outputdir", type=str, help="enter output directory")
     args = parser.parse_args()
     print(f"args: {args}", flush=True)
 
-    print(f"Compare: {args.xkey} and {args.ykey}")
-    print("File Euclidean MAE Pearson")
+    xkey = args.xkey
+    ykey = args.ykey
+
+    import re
+
+    # Todo change title and ... if tdms data
+
+    import re
+
+    labels = [xkey, ykey]
+    for i, label in enumerate(labels):
+        regex = r"(.*?)/"  # before
+        data = re.findall(regex, label)
+        if data:
+            labels[i] = label.replace(f"{data[0]}/", "")
+
+    print(f"Compare: {xkey} and {ykey}")
     tables = []
     headers = [
         "Name",
@@ -72,14 +93,18 @@ if __name__ == "__main__":
         "MAE",
         "Pearson",
         "Image",
-        f"<{args.ykey} - {args.ykey}>",
-        f"min({args.ykey} - {args.ykey})",
-        f"max({args.ykey} - {args.ykey})",
-        f"var({args.ykey} - {args.ykey})",
+        f"<{labels[1]} - {labels[0]}>",
+        f"min",
+        f"max",
+        f"var",
     ]
 
-    supported_formats = [".txt"]
-    for file in args.input_file:
+    if args.outputdir:
+        output = args.outputdir
+
+    supported_formats = [".txt", ".tdms"]
+    input_files = natsorted(args.input_file)
+    for file in input_files:
         f_extension = os.path.splitext(file)[-1]
         if f_extension not in supported_formats:
             raise RuntimeError(
@@ -101,24 +126,33 @@ if __name__ == "__main__":
         match f_extension:
             case ".txt":
                 mrun = MagnetRun.fromtxt(site, insert, file)
+            case ".tdms":
+                mrun = MagnetRun.fromtdms(site, insert, file)
             case _:
                 raise RuntimeError(
                     f"so far file with extension in {supported_formats} are implemented"
                 )
 
-        x = mrun.getData(args.xkey).to_numpy().reshape(-1)
-        y = mrun.getData(args.ykey).to_numpy().reshape(-1)
+        x = mrun.getData(xkey).to_numpy().reshape(-1)
+        y = mrun.getData(ykey).to_numpy().reshape(-1)
         # print('Ib:', x, type(x), x.shape)
         scipy_stats = stats.describe(y - x)
         # print(scipy_stats.minmax) # minmax: tuple, mean, variance
 
         my_ax = plt.gca()
-        mrun.getMData().plotData(x="t", y=args.xkey, ax=my_ax)
-        mrun.getMData().plotData(x="t", y=args.ykey, ax=my_ax)
+        mrun.getMData().plotData(x="t", y=xkey, ax=my_ax)
+        mrun.getMData().plotData(x="t", y=ykey, ax=my_ax)
 
-        imagefile = f"{os.path.splitext(file)[0]}-{args.xkey}-{args.ykey}"
+        dirname = os.path.dirname(file)
+        imagefile = f"{os.path.splitext(file)[0].replace(f_extension, '')}-{labels[0]}-{labels[1]}"
+        if args.outputdir:
+            print(f"imagefile: {imagefile}, ", end="")
+            print(f"replace {dirname} by {args.outputdir}, ", end="")
+            imagefile = imagefile.replace(dirname, args.outputdir)
+            print(f"-> {imagefile}")
+
         table = [
-            file,
+            filename.replace(f_extension, ""),
             mrun.getMData().getDuration(),
             calc_euclidean(x, y),
             calc_mape(x, y),
@@ -131,7 +165,7 @@ if __name__ == "__main__":
         ]
         tables.append(table)
 
-        plt.title(f"{os.path.basename(file)}: {args.xkey}, {args.ykey}")
+        plt.title(f"{filename.replace(f_extension, '')}: {labels[0]}, {labels[1]}")
         if args.save:
             print(f"saveto: {imagefile}_vs_time.png", flush=True)
             plt.savefig(f"{imagefile}_vs_time.png", dpi=300)
@@ -139,15 +173,121 @@ if __name__ == "__main__":
             plt.show()
         plt.close()
 
-        """
-        # dtw algo
-        from dtaidistance import dtw
-        from dtaidistance import dtw_visualisation as dtwvis
-        d, paths = dtw.warping_paths(x, y, window=25, psi=2)
-        best_path = dtw.best_path(paths)
-        dtwvis.plot_warpingpaths(x, y, paths, best_path)
-        print(d)
-        plt.show()
-        """
+        if args.lagcorrelation:
+            (ysymbol, yunit) = mrun.getUnit(ykey)
+            ylabel = f"{ysymbol} [{yunit:~P}]"
+            ymax = y.max()
+
+            (xsymbol, xunit) = mrun.getUnit(xkey)
+            xlabel = f"{xsymbol} [{xunit:~P}]"
+            xmax = x.max()
+
+            y /= abs(ymax)
+            ylabel = f"{labels[1]}: mean={np.mean(y):.3f}, {ymax:.3f} [{yunit:~P}]"
+            if "/" in xkey:
+                (group, channel) = xkey.split("/")
+                if channel != "t":
+                    x /= abs(xmax)
+                    xlabel = f"{labels[0]}: mean={np.mean(x):.3f}, max={xmax:.3f} [{xunit:~P}]"
+            else:
+                if xkey != "t":
+                    x /= abs(xmax)
+                    xlabel = f"{labels[0]}: mean={np.mean(x):.3f}, max={xmax:.3f} [{xunit:~P}]"
+
+            # investigate lag correlation
+            from scipy import signal
+
+            correlation = signal.correlate(x - np.mean(x), y - np.mean(y), mode="full")
+            lags = signal.correlation_lags(len(x), len(y), mode="full")
+            lag = lags[np.argmax(abs(correlation))]
+
+            plt.figure()
+            plt.plot(x - np.mean(x), "-")
+            plt.plot(y - np.mean(y), "-")
+            plt.legend([xlabel, ylabel])
+            plt.xlabel("t [s]")
+            plt.grid()
+            plt.title(
+                f"{os.path.basename(file)}: {labels[0]}, {labels[1]} - cross-correlation"
+            )
+            if args.save:
+                print(f"saveto: {imagefile}_vs_lagcorrelation.png", flush=True)
+                plt.savefig(f"{imagefile}_vs_lagcorrelation.png", dpi=300)
+            else:
+                plt.show()
+            plt.close()
+
+        if args.dtw:
+            # dtw algo
+            from dtaidistance import dtw
+            from dtaidistance import dtw_visualisation as dtwvis
+
+            try:
+                d, paths = dtw.warping_paths(x, y, window=25, psi=2)
+                best_path = dtw.best_path(paths)
+                dtwvis.plot_warpingpaths(x, y, paths, best_path)
+                print(f"dtw distance: {d}")
+                if args.save:
+                    print(f"saveto: {imagefile}_vs_dtw.png", flush=True)
+                    plt.savefig(f"{imagefile}_vs_dtw.png", dpi=300)
+                else:
+                    plt.show()
+            except MemoryError as e:
+                print("!!! retry pyts piecewise aggregate approx !!!")
+
+                # PAA transformation
+                from pyts.approximation import PiecewiseAggregateApproximation
+
+                n_samples, n_timestamps = 1, y.shape[0]
+                window_size = 125
+                paa = PiecewiseAggregateApproximation(
+                    window_size=window_size, overlapping=False
+                )
+                try:
+                    x_paa = paa.transform(np.array([x]))
+                    y_paa = paa.transform(np.array([y]))
+
+                    # Show the results for the first time series
+                    plt.plot(x, "o--", ms=4, label="Original")
+                    plt.plot(
+                        np.arange(
+                            window_size // 2,
+                            n_timestamps + window_size // 2,
+                            window_size,
+                        ),
+                        x_paa[0],
+                        "o--",
+                        ms=4,
+                        label="x PAA",
+                    )
+                    plt.ylabel(xlabel)
+                    plt.xlabel("t [s]")
+                    plt.grid()
+                    plt.title(
+                        f"{os.path.basename(file)}: {labels[0]} - PiecewiseAggegateApprox (windows=125)"
+                    )
+                    if args.save:
+                        print(f"saveto: {imagefile}_paa.png", flush=True)
+                        plt.savefig(f"{imagefile}_paa.png", dpi=300)
+                    else:
+                        plt.show()
+                    plt.close()
+
+                    d, paths = dtw.warping_paths(x_paa[0], y_paa[0], window=125, psi=2)
+                    best_path = dtw.best_path(paths)
+                    dtwvis.plot_warpingpaths(x_paa[0], y_paa[0], paths, best_path)
+                    print(f"dtw distance: {d}")
+                    if args.save:
+                        print(f"saveto: {imagefile}_vs_dtw-paa.png", flush=True)
+                        plt.savefig(f"{imagefile}_vs_dtw-paa.png", dpi=300)
+                    else:
+                        plt.show()
+                except Exception as e:
+                    print(f"dtw using paa exception : {e}, type={type(e)}")
+                    pass
+
+            except Exception as e:
+                print(f"dtw exception: {e}, type={type(e)}")
+                pass
 
     print(tabulate(tables, headers, tablefmt="simple"), "\n")
